e0639960d289d948927ba59e0777730a0dabd662
指北 <jonas.ljq@alibaba-inc.com>
2024-04-10 11:51:27 +0800

    add backend config for tf

--- pkg/terraform/files.go
+++ pkg/terraform/files.go
@@ -161,6 +161,13 @@ func (fp *FileProducer) BuildMainTF() map[string]any {
 					"version": fp.Setup.Requirement.Version,
 				},
 			},
+			"backend": map[string]any{
+				"kubernetes": map[string]any{
+					"secret_suffix":     "capi",
+					"namespace":         "kube-system",
+					"in_cluster_config": true,
+				},
+			},
 		},
 		"provider": map[string]any{
 			providerSource[len(providerSource)-1]: fp.Setup.Configuration,
